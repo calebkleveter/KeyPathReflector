c1eac0c72c021552cd197a818514db5583f2fdf0
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-10-09 22:24:02 -0500

    Created KeyPathReflector

--- Sources/KeyPathReflector/KeyPathReflector.swift
+++ Sources/KeyPathReflector/KeyPathReflector.swift
@@ -1,1 +1,30 @@
+import Runtime
 
+public final class KeyPathReflector<T> {
+    private typealias Cache = (keyPaths: [String: PartialKeyPath<T>?], strings: [PartialKeyPath<T>: String?])
+
+    private let properties: [PropertyInfo]
+    private var cache: Cache
+
+    public init(_ type: T.Type = T.self) throws {
+        self.properties = try typeInfo(of: type).properties
+        self.cache = ([:], [:])
+    }
+
+    public func string(for keyPath: PartialKeyPath<T>) -> String? {
+        if let value = self.cache.strings[keyPath] { return value }
+
+        guard let offset = MemoryLayout<T>.offset(of: keyPath) else {
+            self.cache.strings[keyPath] = nil
+            return nil
+        }
+
+        guard let name = self.properties.first(where: { $0.offset == offset })?.name else {
+            self.cache.strings[keyPath] = nil
+            return nil
+        }
+
+        self.cache.strings[keyPath] = name
+        return name
+    }
+}
